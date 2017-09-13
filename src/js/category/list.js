fbb59043ef045f815a888373fa391e231039746b
zhouhua <BruceZhou163@outlook.com>
2017-09-13 19:35:10 +0800

    学科分类列表实现

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,2 +1,10 @@
 require('../common/header.js');
-require('../common/aside.js'); 
+require('../common/aside.js');
+
+/**
+ * 渲染学科列表
+ */
+
+$.get('/v6/category',function(data){
+    $('.table-bordered').append(template('category-list-tpl',data.result));
+});
